8c5a3dd5c6a7fdd7049b063f359d35e7e16d0900
Andrey Sergeev <andrey@chyuck.com>
2019-05-10 10:10:34 -0400

    Updated InfoHelper

--- Agero.Core.SolutionCreator.Console/InfoHelper.cs
+++ Agero.Core.SolutionCreator.Console/InfoHelper.cs
@@ -8,78 +8,28 @@ namespace Agero.Core.SolutionCreator.Console
         {
             var sb = new StringBuilder();
 
-            const string APPLICATION_NAME = "SolutionCreator";
-
             sb.AppendLine();
-            sb.Append("***** ");
-            sb.Append(APPLICATION_NAME);
-            sb.AppendLine(" *****");
-
-            const int PARAGRAPH_RIGHT_PADDING = 2;
-            var emptySpacesForParagraph = new string(' ', PARAGRAPH_RIGHT_PADDING);
-
+            sb.AppendLine("***** Solution Creator *****");
             sb.AppendLine();
             sb.AppendLine("Description:");
-            sb.Append(emptySpacesForParagraph);
-            sb.AppendLine("Tool for creating initial solution from template by replacing company and");
-            sb.Append(emptySpacesForParagraph);
-            sb.AppendLine("application names in template files and directories.");
-
+            sb.AppendLine("  Tool for creating initial solution from template by replacing company and");
+            sb.AppendLine("  application names in template files and directories.");
             sb.AppendLine();
             sb.AppendLine("Usage:");
-            sb.Append(emptySpacesForParagraph);
-            sb.Append(APPLICATION_NAME);
-            sb.AppendLine(" company_name app_name template_dir_path [-d destination]");
-            sb.Append(emptySpacesForParagraph);
-            sb.AppendLine("                [-a app_name_placeholder] [-c company_name_placeholder]");
-
+            sb.AppendLine("  SolutionCreator company_name app_name template_dir_path [-d destination]");
+            sb.AppendLine("                  [-a app_name_placeholder] [-c comp_name_placeholder]");
             sb.AppendLine();
             sb.AppendLine("Parameters:");
-
-            const int FIRST_COLUMN_RIGHT_PADDING = 26;
-            const int SECOND_COLUMN_RIGHT_PADDING = 10;
-
-            var required = "Required".PadRight(SECOND_COLUMN_RIGHT_PADDING);
-            var optional = "Optional".PadRight(SECOND_COLUMN_RIGHT_PADDING);
-
-            var emptyStringBeforeThirdColumn = new string(' ', PARAGRAPH_RIGHT_PADDING + FIRST_COLUMN_RIGHT_PADDING + SECOND_COLUMN_RIGHT_PADDING);
-
-            sb.Append(emptySpacesForParagraph);
-            sb.Append("company_name".PadRight(FIRST_COLUMN_RIGHT_PADDING));
-            sb.Append(required);
-            sb.AppendLine("Company name, ex. 'Agero'.");
-
-            sb.Append(emptySpacesForParagraph);
-            sb.Append("app_name".PadRight(FIRST_COLUMN_RIGHT_PADDING));
-            sb.Append(required);
-            sb.AppendLine("Application name, ex. 'CaseAPI'.");
-
-            sb.Append(emptySpacesForParagraph);
-            sb.Append("template_dir_path".PadRight(FIRST_COLUMN_RIGHT_PADDING));
-            sb.Append(required);
-            sb.AppendLine("Solution template directory path.");
-
-            sb.Append(emptySpacesForParagraph);
-            sb.Append("destination".PadRight(FIRST_COLUMN_RIGHT_PADDING));
-            sb.Append(optional);
-            sb.AppendLine("Directory to copy output files and");
-            sb.Append(emptyStringBeforeThirdColumn);
-            sb.AppendLine("directories. Default is current directory.");
-
-            sb.Append(emptySpacesForParagraph);
-            sb.Append("app_name_placeholder".PadRight(FIRST_COLUMN_RIGHT_PADDING));
-            sb.Append(optional);
-            sb.AppendLine("Application name placeholder to be used in");
-            sb.Append(emptyStringBeforeThirdColumn);
-            sb.AppendLine("template. Default is 'APP__NAME'.");
-
-            sb.Append(emptySpacesForParagraph);
-            sb.Append("company_name_placeholder".PadRight(FIRST_COLUMN_RIGHT_PADDING));
-            sb.Append(optional);
-            sb.AppendLine("Company name placeholder to be used in");
-            sb.Append(emptyStringBeforeThirdColumn);
-            sb.AppendLine("template. Default is 'COM__NAME'.");
-
+            sb.AppendLine("  company_name          Required  Company name, ex. 'Agero'.");
+            sb.AppendLine("  app_name              Required  Application name, ex. 'CaseAPI'.");
+            sb.AppendLine("  template_dir_path     Required  Solution template directory path.");
+            sb.AppendLine("  destination           Optional  Directory to copy output directories and");
+            sb.AppendLine("                                  files. Default is current directory.");
+            sb.AppendLine("  app_name_placeholder  Optional  Application name placeholder which is");
+            sb.AppendLine("                                  used in template. Default is 'APP__NAME'.");
+            sb.AppendLine("  comp_name_placeholder Optional  Company name placeholder which is");
+            sb.AppendLine("                                  template. Default is 'COM__NAME'.");
+            
             return sb.ToString();
         }
     }
