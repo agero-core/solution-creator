88f770238a8a30aee05cd6ba4793667ccd439233
Andrey Sergeev <andrey@chyuck.com>
2019-06-05 09:17:22 -0400

    Initial implementation Solution Creator (#1)
    * Implemented FileHelper and InfoHelper
    
    * Updated InfoHelper
    
    * Completed implementation with a help of CommandLineParser
    
    * Added dist folder and test
    
    * Updated documentation
    
    * Moved readme.md to root
    
    * Formatted readme.md
    
    * Update README.md

---
+++ Agero.Core.SolutionCreator.Console/Options.cs
@@ -0,0 +1,53 @@
+﻿using CommandLine;
+using System.IO;
+
+namespace Agero.Core.SolutionCreator.Console
+{
+    internal class Options
+    {
+        public Options(string companyName, string applicationName, string templateDirectoryPath, 
+            string outputDestination, string applicationNamePlaceHolder, string companyNamePlaceHolder)
+        {
+            CompanyName = companyName;
+            ApplicationName = applicationName;
+            TemplateDirectoryPath = templateDirectoryPath;
+            OutputDestination = outputDestination ?? Directory.GetCurrentDirectory();
+            ApplicationNamePlaceHolder = applicationNamePlaceHolder ?? DEFAULT_APPLICATION_NAME_PLACEHOLDER;
+            CompanyNamePlaceHolder = companyNamePlaceHolder ?? DEFAULT_COMPANY_NAME_PLACEHOLDER;
+        }
+
+        [Value(0, MetaName = "company_name", 
+            HelpText = "Company name, ex. 'Agero'.", 
+            Hidden = false, Required = true)]
+        public string CompanyName { get; }
+
+        [Value(1, MetaName = "app_name", 
+            HelpText = "Application name, ex. 'TestAPI'.",
+            Hidden = false, Required = true)]
+        public string ApplicationName { get; }
+
+        [Value(2, MetaName = "template_dir_path", 
+            HelpText = "Solution template directory path.",
+            Hidden = false, Required = true)]
+        public string TemplateDirectoryPath { get; }
+
+        [Option('d', MetaValue = "output_destination", 
+            HelpText = "Directory to copy output directories and files. Default is current directory.",
+            Hidden = false, Required = false)]
+        public string OutputDestination { get; }
+
+        public const string DEFAULT_APPLICATION_NAME_PLACEHOLDER = "APP__NAME";
+
+        [Option('a', MetaValue = "app_name_placeholder", 
+            HelpText = "Application name placeholder which is used in template. Default is '" + DEFAULT_APPLICATION_NAME_PLACEHOLDER + "'.",
+            Hidden = false, Required = false)]
+        public string ApplicationNamePlaceHolder { get; }
+
+        public const string DEFAULT_COMPANY_NAME_PLACEHOLDER = "COMP__NAME";
+
+        [Option('c', MetaValue = "comp_name_placeholder", 
+            HelpText = "Company name placeholder which is used in template. Default is '" + DEFAULT_COMPANY_NAME_PLACEHOLDER + "'.",
+            Hidden = false, Required = false)]
+        public string CompanyNamePlaceHolder { get; }
+    }
+}
---
+++ Agero.Core.SolutionCreator.Console/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.IO;
+using Agero.Core.Checker;
+using CommandLine;
+using static System.Console;
+
+namespace Agero.Core.SolutionCreator.Console
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            try
+            {
+                Parser.Default.ParseArguments<Options>(args).WithParsed(Execute);
+            }
+            catch (Exception ex)
+            {
+                WriteLine("Unexpected error:");
+                WriteLine(ex.ToString());
+            }
+        }
+
+        private static void Execute(Options options)
+        {
+            Check.ArgumentIsNull(options, nameof(options));
+            Check.ArgumentIsNull(options.CompanyName, nameof(options.CompanyName));
+            Check.ArgumentIsNull(options.ApplicationName, nameof(options.ApplicationName));
+            Check.ArgumentIsNull(options.TemplateDirectoryPath, nameof(options.TemplateDirectoryPath));
+            Check.Argument(Directory.Exists(options.TemplateDirectoryPath), "Template directory does not exist.");
+            Check.ArgumentIsNull(options.OutputDestination, nameof(options.OutputDestination));
+            Check.ArgumentIsNull(options.ApplicationNamePlaceHolder, nameof(options.ApplicationNamePlaceHolder));
+            Check.ArgumentIsNull(options.CompanyNamePlaceHolder, nameof(options.CompanyNamePlaceHolder));
+
+            var outputDirectoryPath = Path.Combine(options.OutputDestination, DateTime.Now.ToString("yyyyMMddhhmmss"));
+
+            if (!Directory.Exists(outputDirectoryPath))
+                Directory.CreateDirectory(outputDirectoryPath);
+
+            FileHelper.CopyDirectory(options.TemplateDirectoryPath, outputDirectoryPath);
+            FileHelper.ReplaceNameAndTextInDirectoriesAndFiles(outputDirectoryPath, options.CompanyNamePlaceHolder, options.CompanyName);
+            FileHelper.ReplaceNameAndTextInDirectoriesAndFiles(outputDirectoryPath, options.ApplicationNamePlaceHolder, options.ApplicationName);
+
+            WriteLine($"Solution created at {outputDirectoryPath}.");
+        }
+    }
+}
